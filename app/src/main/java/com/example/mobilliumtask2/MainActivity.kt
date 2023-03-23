af083a5414cf08faec3ba2eabbe7e115966a8460
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-03-23 11:47:36 +0300

    detail screen designed

--- app/src/main/java/com/example/mobilliumtask2/MainActivity.kt
+++ app/src/main/java/com/example/mobilliumtask2/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.mobilliumtask2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobilliumtask2.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            var intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
